a6784df3fa2e8d6e00dfc7386e62d9344430fe7c
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-25 14:53:39 -0500

    fixe pin

--- j5DS18B20/temperature-ds18b20.js
+++ j5DS18B20/temperature-ds18b20.js
@@ -6,10 +6,7 @@ var board = new five.Board({
 
 board.on("ready", function() {
   // This requires OneWire support using the ConfigurableFirmata
-  var thermometer = new five.Thermometer({
-    controller: "DS18B20",
-    pin: "P1-11"
-  });
+  var thermometer = new five.Thermometer("P1-11");
 
   thermometer.on("change", function() {
     console.log(this.celsius + "°C");

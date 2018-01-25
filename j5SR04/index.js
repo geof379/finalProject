cb9579f7216e67af45c4cda055fa200540b31c05
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-25 15:34:47 -0500

    test proximity

---
+++ j5SR04/index.js
@@ -0,0 +1,17 @@
+var five = require('johnny-five');
+var PiIO = require('pi-io');
+
+var board = new five.Board({
+  io: new PiIO()
+});
+
+board.on('ready', function() {
+  var proximity = new five.Proximity({
+    controller: 'HCSR04',
+    pin: 'GPIO25'
+  });
+
+  proximity.on("data", function() {
+    console.log("cm: ", this.cm);
+  });
+});

6b089e06b3f71808d4e61d791b43de8bbd13c3c9
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-25 15:01:37 -0500

    changed raspi io by io

--- j5DS18B20/temperature-ds18b20.js
+++ j5DS18B20/temperature-ds18b20.js
@@ -1,12 +1,16 @@
 var five = require("johnny-five");
-var Raspi = require('raspi-io');
+//var Raspi = require('raspi-io');
+var PiIO = require('pi-io');
 var board = new five.Board({
-  io: new Raspi()
+  io: new PiIO()
 });
 
 board.on("ready", function() {
   // This requires OneWire support using the ConfigurableFirmata
-  var thermometer = new five.Thermometer("P1-11");
+  var thermometer = new five.Thermometer({
+    controller: "DS18B20",
+    pin: "GPIO17"
+  });
 
   thermometer.on("change", function() {
     console.log(this.celsius + "°C");

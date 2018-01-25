4ef2e803394e1ad58f7f81bec2c785f7168e2051
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-25 17:44:03 -0500

    buttonLed

--- j5ButtonLED/index.js
+++ j5ButtonLED/index.js
@@ -8,8 +8,8 @@ var board = new five.Board({
 });
 
 board.on('ready', function() {
-  var led = new five.Led('GPIO4');
-  var button = new five.Button('GPIO17');
+  var led = new five.Led('GPIO17');
+  var button = new five.Button('GPIO4');
 
   button.on('down', function() {
     led.on();

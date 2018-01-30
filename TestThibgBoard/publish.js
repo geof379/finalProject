d147a75c9090bd9cf69052e2ad2920f9c2c737ba
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-30 18:20:24 -0500

    test thingboard

---
+++ TestThibgBoard/publish.js
@@ -0,0 +1,16 @@
+var mqtt = require('mqtt');
+
+console.log('Connecting to: %s using access token: %s', process.env.THINGSBOARD_HOST, process.env.ACCESS_TOKEN);
+
+var client  = mqtt.connect('mqtt://'+ 'demo.thingsboard.io',{
+    username: 'DkiMfuhVrpaIWjdW5zbR'
+});
+
+client.on('connect', function () {
+    console.log('Client connected!');
+    client.publish('v1/devices/me/attributes', process.env.ATTRIBUTES);
+    console.log('Attributes published!');
+    client.publish('v1/devices/me/telemetry', process.env.TELEMETRY);
+    console.log('Telemetry published!');
+    client.end();
+});

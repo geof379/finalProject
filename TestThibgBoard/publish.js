78ac27e8cc0f6590592c167da08e30a408779f7f
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-30 18:26:56 -0500

    update publish.js

--- TestThibgBoard/publish.js
+++ TestThibgBoard/publish.js
@@ -8,9 +8,9 @@ var client  = mqtt.connect('mqtt://'+ 'demo.thingsboard.io',{
 
 client.on('connect', function () {
     console.log('Client connected!');
-    client.publish('v1/devices/me/attributes', process.env.ATTRIBUTES);
+    client.publish('v1/devices/me/attributes', {"firmware_version":"1.0.1", "serial_number":"SN-001"});
     console.log('Attributes published!');
-    client.publish('v1/devices/me/telemetry', process.env.TELEMETRY);
+    client.publish('v1/devices/me/telemetry', {"temperature":21, "humidity":55.0, "active": false});
     console.log('Telemetry published!');
     client.end();
 });

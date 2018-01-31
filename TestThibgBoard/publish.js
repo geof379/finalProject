50b03fe0328e41a9af84fc6283d7e1f54b87d32c
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-30 19:01:15 -0500

    update publish.js

--- TestThibgBoard/publish.js
+++ TestThibgBoard/publish.js
@@ -10,7 +10,7 @@ client.on('connect', function () {
     console.log('Client connected!');
     var n = 0;
     while(n<100){
-        client.publish('v1/devices/me/attributes', {"firmware_version":""+n, "serial_number":"SN-001"});
+        client.publish('v1/devices/me/attributes', "{'firmware_version':n, 'serial_number':'SN-001'}");
         client.publish('v1/devices/me/telemetry', {"temperature":n, "humidity":53.0+n, "active": n%2===0});
         console.log('Data published!');
     }

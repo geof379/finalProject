c079dbe6d2bf2dfd8ec5f6d412132335b38c9e55
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-30 21:43:29 -0500

    dht11 test#

--- temperature_dht11/index.js
+++ temperature_dht11/index.js
@@ -3,9 +3,9 @@ var mqtt = require('mqtt');
 
 console.log('Connecting to: %s using access token: %s', 'demo.thingsboard.io', 'DkiMfuhVrpaIWjdW5zbR');
 
-//var client  = mqtt.connect('mqtt://'+ 'demo.thingsboard.io',{
-  //  username: 'DkiMfuhVrpaIWjdW5zbR'
-//});
+var client  = mqtt.connect('mqtt://'+ 'demo.thingsboard.io',{
+   username: 'DkiMfuhVrpaIWjdW5zbR'
+});
 
 var sensor_data = [];
 var sensor = {
@@ -14,23 +14,25 @@ var sensor = {
         type: 11,
         pin: 18
     } ],
-    read: function() {
-        for (var a in this.sensors) {
-            var b = sensorLib.read(this.sensors[a].type, this.sensors[a].pin);
+
+};
+
+client.on('connect', function () {
+    console.log('Client connected!');
+
+
+        for (var a in sensor.sensors) {
+            var b = sensorLib.read(sensor.sensors[a].type, sensor.sensors[a].pin);
             sensor_data['temperature'] = b.temperature.toFixed(2);
             sensor_data['humidity'] = b.humidity.toFixed(2);
             console.log(sensor_data);
-            //client.publish('v1/devices/me/telemetry', JSON.stringify(sensor_data));
+            client.publish('v1/devices/me/attributes', sensor_data);
+            client.publish('v1/devices/me/telemetry',sensor_data,1);
             console.log('Data published!');
         }
-    }
-};
-
-
-setTimeout(function() {
-    sensor.read();
-}, 2000);
 
 
 
-sensor.read();
+
+    client.end();
+});

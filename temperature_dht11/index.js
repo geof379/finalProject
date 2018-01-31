a1470f99ab813cfc28f9c56750876f6984c79572
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-30 22:00:08 -0500

    dht11 test#

--- temperature_dht11/index.js
+++ temperature_dht11/index.js
@@ -17,22 +17,18 @@ var sensor = {
 
 };
 
-client.on('connect', function () {
-    console.log('Client connected!');
-
-
-        for (var a in sensor.sensors) {
-            var b = sensorLib.read(sensor.sensors[a].type, sensor.sensors[a].pin);
-            sensor_data['temperature'] = b.temperature.toFixed(2);
-            sensor_data['humidity'] = b.humidity.toFixed(2);
-            console.log(JSON.stringify(sensor_data));
-            client.publish('v1/devices/me/attributes', JSON.stringify(sensor_data));
-            client.publish('v1/devices/me/telemetry',JSON.stringify(sensor_data),1);
-            console.log('Data published!');
-        }
-
-
-
-
-    client.end();
-});
+while(true){
+    client.on('connect', function () {
+        console.log('Client connected!');
+            for (var a in sensor.sensors) {
+                var b = sensorLib.read(sensor.sensors[a].type, sensor.sensors[a].pin);
+                sensor_data['temperature'] = b.temperature.toFixed(2);
+                sensor_data['humidity'] = b.humidity.toFixed(2);
+                console.log(JSON.stringify(sensor_data));
+                client.publish('v1/devices/me/attributes', JSON.stringify(sensor_data));
+                client.publish('v1/devices/me/telemetry',JSON.stringify(sensor_data),1);
+                console.log('Data published!');
+            }
+        client.end();
+    });
+}

c27551fec9b5b84c593429e88015dbd0ce4dde79
Ayeboua <folly-tata.ayeboua.1@ens.etsmtl.ca>
2018-01-30 22:02:24 -0500

    dht11 test#

--- temperature_dht11/index.js
+++ temperature_dht11/index.js
@@ -17,10 +17,12 @@ var sensor = {
 
 };
 
-while(true){
-    client.on('connect', function () {
-        console.log('Client connected!');
-            for (var a in sensor.sensors) {
+
+client.on('connect', function () {
+    console.log('Client connected!');
+
+    while(n<1000){
+         for (var a in sensor.sensors) {
                 var b = sensorLib.read(sensor.sensors[a].type, sensor.sensors[a].pin);
                 sensor_data['temperature'] = b.temperature.toFixed(2);
                 sensor_data['humidity'] = b.humidity.toFixed(2);
@@ -28,7 +30,9 @@ while(true){
                 client.publish('v1/devices/me/attributes', JSON.stringify(sensor_data));
                 client.publish('v1/devices/me/telemetry',JSON.stringify(sensor_data),1);
                 console.log('Data published!');
-            }
-        client.end();
-    });
-}
+        }
+        n++;
+    }
+
+    client.end();
+});
